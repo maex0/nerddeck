9ec3c1b5025b6e712b251b26ef69b6235441add7
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-12-03 22:10:43 +0100

    added io and greeting strings

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -5,8 +5,45 @@ import Model.Deck
 import Model.Session
 import SM2
 
+import Data.Char (ord)
+
+nerdDeckASCII :: String
+nerdDeckASCII =
+    "     _   __              ______            __  \n" ++
+    "    / | / /__  _________/ / __ \\___  _____/ /__\n" ++
+    "   /  |/ / _ \\/ ___/ __  / / / / _ \\/ ___/ //_/\n" ++
+    "  / /|  /  __/ /  / /_/ / /_/ /  __/ /__/ ,<   \n" ++
+    " /_/ |_/\\___/_/   \\__,_/_____/\\___/\\___/_/|_|  \n"   
+
+mainMenu :: IO (Int)
+mainMenu = do
+  putStrLn "\nOptions:"
+  putStrLn "1. Add Flash Card"
+  putStrLn "2. View Flash Cards"
+  putStrLn "3. Start Learning"
+  putStrLn "4. Exit"
+  option <- getLine
+  putStrLn $ "Your choice: " ++ option
+  -- here map option to values
+  -- chartoint
+  return 1
+
+endlessMenu :: IO()
+endlessMenu = do
+    mainMenu
+    endlessMenu
+
+    
+
+
 main :: IO ()
 main = do
+    putStrLn $ "\nWelcome to\n" ++ nerdDeckASCII
+    putStrLn "Developed by Maximilian Gobbel"
+    putStrLn "If you want to know more about NerdDeck, visit https://github.com/maex0/nerddeck"
+
+    endlessMenu
+  
     let sampleCard1 = Flashcard 1 "Question" "Answer" 1 Ease1 0 (Deck 1 "Math")
     
     let initialDeck = addToDeck [] sampleCard1
