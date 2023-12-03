5c6e2730ed11335ff0461ed55316c1337f19cef5
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-12-03 11:21:40 +0100

    added session, ease and add and review functions

--- haskell/SM2.hs
+++ haskell/SM2.hs
@@ -3,7 +3,7 @@ data Flashcard = Flashcard
     , question :: String
     , answer :: String
     , interval :: Int
-    , ease :: Int
+    , ease :: Ease
     , reviewDate :: Int -- todo: This should be a real date later
     , deck :: Deck
     } deriving (Show)
@@ -13,7 +13,65 @@ data Deck = Deck
     , name :: String
     } deriving (Show)
 
+data Ease = Ease1 | Ease2 | Ease3 | Ease4 | Ease5 deriving (Show)
+
+easeLevel :: Ease -> Int
+easeLevel e = case e of
+  Ease1 -> 1
+  Ease2 -> 2
+  Ease3 -> 3
+  Ease4 -> 4
+  Ease5 -> 5
+
+initialInterval = 1
+
+presentFlashcard :: Flashcard -> Flashcard
+presentFlashcard card = card { interval = 1 }
+
+type FlashcardDeck = [Flashcard]
+
+addToDeck :: FlashcardDeck -> Flashcard -> FlashcardDeck
+addToDeck deck card = card : deck
+
+
+reviewFlashcard :: Flashcard -> Bool -> Flashcard
+reviewFlashcard card success =
+    if success
+        then card { interval = nextInterval (interval card) }
+        else card { interval = 1 }
+
+nextInterval :: Int -> Int
+nextInterval currentInterval = currentInterval * 2
+
+
+reviewDeck :: FlashcardDeck -> Bool -> FlashcardDeck
+reviewDeck deck success = map (\card -> reviewFlashcard card success) deck
+
+
+data Session = Session
+    { sessionDeck :: FlashcardDeck
+    , sessionSuccess :: Bool
+    } deriving (Show)
+
+startNewSession :: FlashcardDeck -> Session
+startNewSession deck = Session { sessionDeck = deck, sessionSuccess = False }
+
+endSession :: Session -> FlashcardDeck
+endSession session = reviewDeck (sessionDeck session) (sessionSuccess session)
+
+
 
 main :: IO ()
-main = print $ Deck 0 "Math"
- 
+main = do
+    let sampleCard1 = Flashcard 1 "Question" "Answer" 1 Ease1 0 (Deck 1 "Math")
+    let sampleCard2 = Flashcard 2 "Question" "Answer" 1 Ease1 0 (Deck 1 "Math")
+    
+    let initialDeck = addToDeck [] sampleCard1
+    let session = startNewSession initialDeck
+
+    putStrLn "Initial Deck:"
+    mapM_ (putStrLn . show) (sessionDeck session)
+
+    putStrLn "\nEnd Session (Success):"
+    let updatedDeck = endSession session { sessionSuccess = True }
+    mapM_ (putStrLn . show) updatedDeck
