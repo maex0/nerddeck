0340b6e91c1e76a522e668baacb29fbf31f896c3
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-12-15 16:25:22 +0100

    removed magic numbers

--- go/flashcards/flashcard.go
+++ go/flashcards/flashcard.go
@@ -10,8 +10,14 @@ import (
 )
 
 const (
-	defaultRepetitions = 0
-	defaultEFactor     = 2.5
+	defaultRepetition = 0
+	defaultEFactor    = 2.5
+	firstRepetition   = 1
+	secondRepetition  = 2
+	gradeOne          = 1
+	gradeTwo          = 2
+	gradeThree        = 3
+	gradeFour         = 4
 )
 
 type FlashCard struct {
@@ -28,7 +34,7 @@ func NewFlashCard(question, answer string) FlashCard {
 		ID:          GenerateID(question, answer),
 		Question:    question,
 		Answer:      answer,
-		Repetitions: defaultRepetitions,          // Start with 0 for no assumed successful recall
+		Repetitions: defaultRepetition,           // Start with 0 for no assumed successful recall
 		EFactor:     defaultEFactor,              // Default Easiness Factor
 		NextReview:  time.Now().AddDate(0, 0, 0), // Default Next Review Date (1 day in the future)
 	}
@@ -38,19 +44,19 @@ func (card *FlashCard) ApplySM2Algorithm(grade string) {
 	numericGrade := convertGrade(grade)
 
 	// 1. Update repetitions and easiness factor
-	if card.Repetitions == 0 || numericGrade >= 3 {
+	if card.Repetitions == defaultRepetition || numericGrade >= gradeThree {
 		card.Repetitions++
 		card.EFactor = card.EFactor + 0.1 - (5.0-float64(numericGrade))*(0.08+(5.0-float64(numericGrade))*0.02)
 	} else {
-		card.Repetitions = 1
+		card.Repetitions = firstRepetition
 		card.EFactor = 1.3
 	}
 
 	// 2. Calculate the next review interval
-	if card.Repetitions == 1 {
+	if card.Repetitions == firstRepetition {
 		// next review is set to tomorrow if new or grade is 1 or 2
 		card.NextReview = time.Now().AddDate(0, 0, 1)
-	} else if card.Repetitions == 2 {
+	} else if card.Repetitions == secondRepetition {
 		card.NextReview = time.Now().AddDate(0, 0, 6)
 	} else {
 		card.NextReview = time.Now().AddDate(0, 0, int(math.Round(float64(card.Repetitions)*card.EFactor)))
@@ -59,9 +65,9 @@ func (card *FlashCard) ApplySM2Algorithm(grade string) {
 
 func convertGrade(grade string) int {
 	numericGrade, err := strconv.Atoi(grade)
-	if err != nil || numericGrade < 1 || numericGrade > 4 {
-		fmt.Println("Invalid grade. Using default grade 1.")
-		return 1 // Default to the lowest grade
+	if err != nil || numericGrade < gradeOne || numericGrade > gradeFour {
+		fmt.Printf("Invalid grade. Using default grade %d.", gradeOne)
+		return gradeOne // Default to the lowest grade
 	}
 
 	return numericGrade
