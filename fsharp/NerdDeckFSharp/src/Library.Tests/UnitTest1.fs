33a6886798b4e37a5149a09b6c5735742ab12c14
Maximilian Gobbel <maxigobbel@googlemail.com>
2023-12-18 16:59:47 +0100

    added first unit test

--- fsharp/NerdDeckFSharp/src/Library.Tests/UnitTest1.fs
+++ fsharp/NerdDeckFSharp/src/Library.Tests/UnitTest1.fs
@@ -1,11 +1,27 @@
 module Library.Tests
 
 open NUnit.Framework
+open FlashCard
+open System
 
 [<SetUp>]
 let Setup () =
     ()
 
 [<Test>]
-let Test1 () =
-    Assert.Pass()
+let ``FlashCard creation test`` () =
+    let id = "1"
+    let question = "What is the capitol of france?"
+    let answer = "Paris"
+    let repetitions = 0
+    let eFactor = 2.5
+    let nextReview = DateTime.Now
+
+    let card = { ID = id; Question = question; Answer = answer; Repetitions = repetitions; EFactor = eFactor; NextReview = nextReview }
+
+    Assert.AreEqual(id, card.ID)
+    Assert.AreEqual(question, card.Question)
+    Assert.AreEqual(answer, card.Answer)
+    Assert.AreEqual(repetitions, card.Repetitions)
+    Assert.AreEqual(eFactor, card.EFactor)
+    Assert.AreEqual(nextReview, card.NextReview)
